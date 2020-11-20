7ea51c6e6c6b992925425ac8774924cd2394f93f
boneill <brian.oneill@seedim.com.au>
2020-11-20 15:48:50 +1100

    moved job to 62

--- temp/amc-repo/src/main/java/au/org/amc/repo/webscripts/base/Validator.java
+++
@@ -1,7 +0,0 @@
-package au.org.amc.repo.webscripts.base;
-
-import java.util.Map;
-
-public interface Validator {
-  void validate(Map<String, String> map) throws ValidationException;
-}
--- temp/amc-share/src/main/resources/alfresco/web-extension/site-webscripts/au/org/amc/common.js
+++
@@ -1,8 +0,0 @@
-function packageInResponseJSON(success, responseCode, responseMessage, payLoad) {
-	var response = {};
-	response.responseCode = responseCode;
-	response.responseMessage = responseMessage;
-	response.payLoad = payLoad;
-	response.success = success;
-	return jsonUtils.toJSONString(response);
-}
