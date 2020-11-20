30c9b82e8a17f1dc5cb9b81ecbfe6f1471676ebe
boneill <brian.oneill@seedim.com.au>
2020-11-20 17:11:30 +1100

    Change to job to allow user to be set in context

--- alfresco-sync-platform/src/main/java/au/com/seedim/synch/repo/job/SynchJobDetails.java
+++ alfresco-sync-platform/src/main/java/au/com/seedim/synch/repo/job/SynchJobDetails.java
@@ -3,6 +3,8 @@ package au.com.seedim.synch.repo.job;
 import org.alfresco.error.AlfrescoRuntimeException;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.schedule.AbstractScheduledLockedJob;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -11,14 +13,21 @@ import org.quartz.StatefulJob;
 public class SynchJobDetails extends AbstractScheduledLockedJob implements StatefulJob{
   
   
+	private static Log logger = LogFactory.getLog(SynchJobDetails.class);
+	
   @Override
   public void executeJob(JobExecutionContext context)
       throws JobExecutionException {
     // TODO Auto-generated method stub
     
-    
+	  
+	  
     JobDataMap jobData = context.getJobDetail().getJobDataMap();
-
+    
+    String runAsUser = (String)jobData.get("runAsUserName");
+    logger.debug("runAsUser:" + runAsUser);
+    
+    
     // Extract the Job executer to use
     Object executerObj = jobData.get("jobExecuter");
     if (executerObj == null || !(executerObj instanceof SynchJobExecutor)) {
@@ -34,7 +43,7 @@ public class SynchJobDetails extends AbstractScheduledLockedJob implements State
             jobExecuter.execute();
             return null;
         }
-    }, "alfresco.synch");
+    }, runAsUser);
   }
 
 
